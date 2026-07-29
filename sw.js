f557a9d3f85acf2aa1dfad6cdfa112742e644cae
MATCH0618 <tanakayuuma0618@gmail.com>
2026-07-30 08:45:19 +0900

    Refresh PWA cache for timer fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "manabi-partner-v5";
+const CACHE = "manabi-partner-v6";
 const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
