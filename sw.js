64b6bcfa1b0eba7c07e4107f2b09b2a1a1c71661
MATCH0618 <tanakayuuma0618@gmail.com>
2026-07-30 08:55:41 +0900

    Refresh app cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "manabi-partner-v6";
+const CACHE = "manabi-partner-v7";
 const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
